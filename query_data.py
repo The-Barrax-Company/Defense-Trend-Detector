32adf83cc170c34947a5836bc537f273cc2c35ad
Mark Faust <markfaust123@gmail.com>
2025-02-22 16:21:06 -0500

    modified output and import statement for clearer querying

--- query_data.py
+++ query_data.py
@@ -1,5 +1,5 @@
 import argparse
-from langchain.vectorstores.chroma import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.llms.ollama import Ollama
 from get_embedding_function import get_embedding_function
@@ -37,13 +37,13 @@ def query_rag(query_text: str):
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-    # print(prompt)
+    print(prompt, '\n')
 
     model = Ollama(model="mistral")
     response_text = model.invoke(prompt)
 
     sources = [doc.metadata.get("id", None) for doc, _score in results]
-    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    formatted_response = f"Response: {response_text}\n\nSources: {sources}\n"
     print(formatted_response)
     return response_text
 
